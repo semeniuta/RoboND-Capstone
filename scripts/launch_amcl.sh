2978d9e853c447641403557ae8c63d0a2e868eee
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-06-10 13:36:42 +0200

    Make launch_amcl.sh use the launch file in home_service_robot

--- scripts/launch_amcl.sh
+++ scripts/launch_amcl.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export TURTLEBOT_GAZEBO_MAP_FILE=$HOME/RoboND/catkin_ws/src/amcl_localize/maps/my_world_map.yaml
-
-roslaunch turtlebot_gazebo amcl_demo.launch
+roslaunch home_service_robot amcl_demo.launch
