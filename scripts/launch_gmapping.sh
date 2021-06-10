b5d98fdb57ffce075147821f593c5a2cfd67db31
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-06-10 12:00:12 +0200

    Fine-tune parameters of slam_gmaping

--- scripts/launch_gmapping.sh
+++ scripts/launch_gmapping.sh
@@ -3,4 +3,4 @@
 #export TURTLEBOT_3D_SENSOR=kinect
 #roslaunch turtlebot_gazebo gmapping_demo.launch
 
-rosrun gmapping slam_gmapping
+rosrun gmapping slam_gmapping _angularUpdate:=0.01 _linearUpdate:=0.02 _particles:=250 _map_update_interval:=5.0 _xmin:=-15 _ymin:=-15 _xmax:=15 _ymax:=15 _delta:=0.01
