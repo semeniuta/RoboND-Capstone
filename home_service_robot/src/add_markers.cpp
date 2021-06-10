58730773d44d042705cf7b28fefe4ac707dfe8e3
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-06-10 14:37:44 +0200

    Start adapting add_markers node

--- home_service_robot/src/add_markers.cpp
+++ home_service_robot/src/add_markers.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "basic_shapes");
+    ros::init(argc, argv, "add_markers");
     ros::NodeHandle n;
     ros::Rate r(1);
     ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
@@ -14,16 +14,18 @@ int main(int argc, char **argv)
     // Set our initial shape type to be a cube
     uint32_t shape = visualization_msgs::Marker::CUBE;
 
+    double cube_side = 0.2;
+
     while (ros::ok())
     {
         visualization_msgs::Marker marker;
         // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-        marker.header.frame_id = "/my_frame";
+        marker.header.frame_id = "map";
         marker.header.stamp = ros::Time::now();
 
         // Set the namespace and id for this marker.  This serves to create a unique ID
         // Any marker sent with the same namespace and id will overwrite the old one
-        marker.ns = "basic_shapes";
+        marker.ns = "add_markers";
         marker.id = 0;
 
         // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
@@ -34,22 +36,22 @@ int main(int argc, char **argv)
 
         // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
         marker.pose.position.x = 0;
-        marker.pose.position.y = 0;
-        marker.pose.position.z = 0;
+        marker.pose.position.y = 1;
+        marker.pose.position.z = cube_side / 2.;
         marker.pose.orientation.x = 0.0;
         marker.pose.orientation.y = 0.0;
         marker.pose.orientation.z = 0.0;
         marker.pose.orientation.w = 1.0;
 
         // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
+        marker.scale.x = cube_side;
+        marker.scale.y = cube_side;
+        marker.scale.z = cube_side;
 
         // Set the color -- be sure to set alpha to something non-zero!
         marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
+        marker.color.g = 0.0f;
+        marker.color.b = 1.0f;
         marker.color.a = 1.0;
 
         marker.lifetime = ros::Duration();
@@ -65,23 +67,6 @@ int main(int argc, char **argv)
             sleep(1);
         }
         marker_pub.publish(marker);
-
-        // Cycle between different shapes
-        switch (shape)
-        {
-        case visualization_msgs::Marker::CUBE:
-            shape = visualization_msgs::Marker::SPHERE;
-            break;
-        case visualization_msgs::Marker::SPHERE:
-            shape = visualization_msgs::Marker::ARROW;
-            break;
-        case visualization_msgs::Marker::ARROW:
-            shape = visualization_msgs::Marker::CYLINDER;
-            break;
-        case visualization_msgs::Marker::CYLINDER:
-            shape = visualization_msgs::Marker::CUBE;
-            break;
-        }
 
         r.sleep();
     }
