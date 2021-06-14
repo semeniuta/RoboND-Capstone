509bd17548e64400b1a90bad50454c388700ce21
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2021-06-14 11:10:34 +0200

    Use custom turtlebot_world.launch in launch script

--- scripts/launch_world.sh
+++ scripts/launch_world.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=$HOME/RoboND/catkin_ws/src/my_robot_world/worlds/my.world
-export TURTLEBOT_BASE=kobuki
-export TURTLEBOT_STACKS=hexagons
-export TURTLEBOT_3D_SENSOR=kinect
-
-#xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
-#sleep 5
-#xterm  -e  "roslaunch turtlebot_gazebo gmapping_demo.launch"
-
-roslaunch turtlebot_gazebo turtlebot_world.launch
+roslaunch home_service_robot turtlebot_world.launch
